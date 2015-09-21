0c2857c4815334432ca2e03ed9f837398bbd1ab2
Nathan Robertus <nathan.robertus@gmail.com>
2015-09-21 14:30:56 -0600

    Restructured file system, changed images, updated footer

--- TaCo Site/js/global.js
+++ TaCo Site/js/global.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	toggleContent(null, "home");
+	var elementButton = document.querySelector('.elevator');
+    var elevator = new Elevator({
+        element: elementButton,
+        mainAudio: './music/elevator.mp3', // Music from http://www.bensound.com/
+        endAudio:  './music/ding.mp3'
+    });
 })
 function toggleContent(object, name){
 	$('#body_content').fadeOut(400, function(){
