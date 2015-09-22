9726de3d21b0d3de9cb936b9524d5650f2781ef8
Nathan Robertus <nathan.robertus@gmail.com>
2015-09-21 18:29:55 -0600

    Final commit, code cleanup, directory structure and favicon

--- TaCo Site/js/global.js
+++ TaCo Site/js/global.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-	toggleContent(null, "home");
+	toggleContent(null, "home", 0);
 	var elementButton = document.querySelector('.elevator');
     var elevator = new Elevator({
         element: elementButton,
         mainAudio: './music/elevator.mp3', // Music from http://www.bensound.com/
         endAudio:  './music/ding.mp3'
     });
-})
-function toggleContent(object, name){
-	$('#body_content').fadeOut(400, function(){
+});
+function toggleContent(object, name, fadeTime){
+	fadeTime ? fadeTime = fadeTime : fadeTime = 400;
+	$('#body_content').fadeOut(fadeTime, function(){
 		$('#body_content').html($("#" + name.toLowerCase().replace(" ", "_") + "_content").html());
-		$('#body_content').fadeIn();
+		$('#body_content').fadeIn(fadeTime);
 	});
 }
