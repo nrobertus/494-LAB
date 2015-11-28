a88f0de247729596ccde2aac206edd7c9b1eebdc
Nathan Robertus <nathan.robertus@gmail.com>
2015-11-28 00:36:32 -0700

    Added the final project

---
+++ Final Project/js/global.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	toggleContent(null, "home", 0);
+	var elementButton = document.querySelector('.elevator');
+    var elevator = new Elevator({
+        element: elementButton,
+        mainAudio: './music/elevator.mp3', // Music from http://www.bensound.com/
+        endAudio:  './music/ding.mp3'
+    });
+		$('html').smoothScroll(); // Implement a smooth scrolling library, circumvents the CSS cross-browser limiations
+});
+function toggleContent(object, name, fadeTime){
+	fadeTime ? fadeTime = fadeTime : fadeTime = 400;
+	$('#body_content').fadeOut(fadeTime, function(){
+		$('#body_content').html($("#" + name.toLowerCase().replace(" ", "_") + "_content").html());
+		$('#body_content').fadeIn(fadeTime);
+	});
+}
